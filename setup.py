115c37935604b0bcefb4aea14e10cb9d6e003f66
Niketan Rana <90609628+NikeRana@users.noreply.github.com>
2023-08-08 16:11:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,20 +7,20 @@ with open("README.md", "r", encoding="utf-8") as f:
 REPO_NAME = "Books-Recommender-System-Using-Machine-Learning"
 AUTHOR_USER_NAME = "Nike_Rana"
 SRC_REPO = "src"
-LIST_OF_REQUIREMENTS = ['streamlit', 'numpy']
+LIST_OF_REQUIREMENTS = ['streamlit', 'numpy','scikit-learn']
 
 
 setup(
     name=SRC_REPO,
     version="0.0.1",
     author=AUTHOR_USER_NAME,
-    description="A small package for Movie Recommender System",
+    description="A small package for Book Recommender System",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/NikeRana",
     author_email="cordate084@gmail.com",
     packages=[SRC_REPO],
     license="MIT",
-    python_requires=">=3.7",
+    python_requires=">=3.11",
     install_requires=LIST_OF_REQUIREMENTS
 )
